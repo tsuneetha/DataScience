135b56a831cf646b1208eaeea3cdbcb952b30765
tsuneetha <57337989+tsuneetha@users.noreply.github.com>
2019-11-29 07:10:16 +0530

    Create DeepNeuralNetwork.py

---
+++ DeepNeuralNetwork.py
@@ -0,0 +1,56 @@
+f=open("/home/jovyan/demo/data/patients.txt")
+h=f.readline()
+lines=f.readlines()
+print(len(lines))
+
+x=[]
+y=[]
+for line in lines:
+    w=line.strip().lower().split(",")
+    ins=[float(v) for v in w[1:-1]]
+    x.append(ins)
+    if(w[-1]=="yes"):
+        y.append(1)
+    else:
+        y.append(0)
+print(x)
+print(y)
+
+import numpy as np
+ones=np.ones(len(lines))
+x=np.c_[ones,x]
+y=np.c_[y]
+print(x)
+print(y)
+
+#network Building
+#1 input layer, 4 neurons
+#3 hidden layers, hidden1-6 neurons,hidden2-9 neurons, hidden3-6 neurons
+#1 output layer, 1 neuron
+np.random.seed(100)
+i1=x.shape[1]
+h1=int(i1*1.5)
+h2=int(h1*1.5)
+h3=h1
+o1=1
+w1=2*np.random.random((i1,h1))-1
+w2=2*np.random.random((h1,h2))-1
+w3=2*np.random.random((h2,h3))-1
+w4=2*np.random.random((h3,o1))-1
+print(w1.shape)
+print(w2.shape)
+print(w3.shape)
+print(w4.shape)
+
+#Training DNN
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+def mse(y,ycap):
+    return ((y-ycap)**2).mean()
+def derivative(x):
+    return x*(1-x)
+ploss=0
+flag=0
+conv=1e-8
+for i in range(100000):
+    
